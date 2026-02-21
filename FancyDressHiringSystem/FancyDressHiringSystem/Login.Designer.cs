65d640899da92d8d1c62927e199734b23a0ecf2c
Abdula Afzal <abdulaafzal@icloud.com>
2026-02-21 02:02:17 +0000

    Created a login page

--- FancyDressHiringSystem/FancyDressHiringSystem/Login.Designer.cs
+++ FancyDressHiringSystem/FancyDressHiringSystem/Login.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Login";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            label3 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Palace Script MT", 28.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(590, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(164, 86);
+            label1.TabIndex = 0;
+            label1.Text = "Fancy dress\r\n\r\n";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(603, 177);
+            label2.Name = "label2";
+            label2.Size = new Size(132, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Email or username";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(611, 212);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 24);
+            button1.TabIndex = 2;
+            button1.Text = "text";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(630, 249);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Password";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(611, 283);
+            button2.Name = "button2";
+            button2.Size = new Size(113, 24);
+            button2.TabIndex = 4;
+            button2.Text = "text";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(611, 363);
+            button3.Name = "button3";
+            button3.Size = new Size(113, 44);
+            button3.TabIndex = 5;
+            button3.Text = "LOGIN";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(603, 423);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(128, 20);
+            linkLabel1.TabIndex = 6;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Create an account";
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new Point(611, 321);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(120, 20);
+            linkLabel2.TabIndex = 7;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Forgot password";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(450, 572);
+            panel1.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(450, 572);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            ClientSize = new Size(859, 572);
+            Controls.Add(panel1);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Login";
+            Text = "Login";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Label label3;
+        private Button button2;
+        private Button button3;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private Panel panel1;
+        private PictureBox pictureBox1;
     }
 }
--- FancyDressHiringSystem/FancyDressHiringSystem/Login.cs
+++ FancyDressHiringSystem/FancyDressHiringSystem/Login.cs
@@ -16,5 +16,10 @@ namespace FancyDressHiringSystem
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
